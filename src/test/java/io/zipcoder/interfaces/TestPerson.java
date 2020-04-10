884b03c9340492bb0be608e8e68c894d4925b770
ErikFox <erikmichaelfox@gmail.com>
2020-04-10 16:25:35 -0400

    Add and update majority of structure part 1-6

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,29 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
+
+    @Test
+    public void testConstructor() {
+        // Given
+        Long id = 12571712L;
+        // When
+        Person person = new Person(id, "John");
+        // Then
+        Assert.assertEquals(id, person.getId());
+    }
+
+    @Test
+    public void testSetName() {
+        // Given
+        Person person = new Person(1468942L, "Bob");
+        // When
+        String newName = "Joe";
+        person.setName(newName);
+        // Then
+        Assert.assertEquals(newName, person.getName());
+    }
 
 }
